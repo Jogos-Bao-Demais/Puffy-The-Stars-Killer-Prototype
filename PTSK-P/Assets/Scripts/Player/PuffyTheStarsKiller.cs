21303a6d3e32f2b1d8e7eaa7be672ad764a6216e
gseven-gdv <zerogravitystd@gmail.com>
2023-10-14 19:07:52 -0300

    basics

--- PTSK-P/Assets/Scripts/Player/PuffyTheStarsKiller.cs
+++ PTSK-P/Assets/Scripts/Player/PuffyTheStarsKiller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
-public class PuffyTheStarsKiller : MonoBehaviour
+public partial class PuffyTheStarsKiller : MonoBehaviour, IDamageable
 {
     [Header("References")]
 
@@ -14,15 +14,23 @@ public class PuffyTheStarsKiller : MonoBehaviour
     [SerializeField] private float _friction = .2f;
 
     [Header("Dash")]
+    [SerializeField] private float _dashSpeed;
+    [SerializeField] private float _dashDrag;
+    [SerializeField] private float _dashDuration;
+
+    [Header("Debug")]
+    [SerializeField] private bool _shouldDie = false;
 
     private MainInputActions _inputActions;
     private Rigidbody2D _rigidbody;
 
     private Vector2 _movementVector = Vector2.zero;
-    private Vector2 _dashDirectin = Vector2.zero;
+    private Vector2 _dashDirection = Vector2.zero;
 
     private bool _isDashing;
     private float _currentHealth;
+
+    internal float _lookAngle;
 
     private void Awake()
     {
@@ -37,8 +45,52 @@ public class PuffyTheStarsKiller : MonoBehaviour
         _currentHealth = _maxHealth;
     }
 
+    private void Update()
+    {
+        if (_currentHealth <= 0f) {
+            Die();
+        }
+
+        _movementVector = _inputActions.Player.Move.ReadValue<Vector2>();
+    }
+
     private void FixedUpdate()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        _rigidbody.velocity = _movementVector.x != 0  
+            ? _movementVector.x * _moveSpeed * Time.fixedDeltaTime * Vector2.right + _rigidbody.velocity.y * Vector2.up 
+            : new Vector2(Mathf.Lerp(_rigidbody.velocity.x, 0f, (_friction - 4f) * Time.fixedDeltaTime), _rigidbody.velocity.y);
         
+        _rigidbody.velocity = _movementVector.y != 0  
+            ? _rigidbody.velocity.x * Vector2.right + _movementVector.y * _moveSpeed * Time.fixedDeltaTime * Vector2.up 
+            : new Vector2(_rigidbody.velocity.x, Mathf.Lerp(_rigidbody.velocity.y, 0f, (_friction - 4f) * Time.fixedDeltaTime));
+    }
+
+    private void Die()
+    {
+#if UNITY_EDITOR
+        if (_shouldDie) {
+            Debug.Log("Puffy The Stars Killer Died");
+
+            return;
+        }
+#endif
+
+        Debug.Log("Puffy The Stars Killer Died");
+    }
+
+    public void TakeDemage(float damage)
+    {
+        _currentHealth -= damage;
+
+        if (_currentHealth <= 0) {
+            Die();
+
+            return;
+        }
     }
 }
